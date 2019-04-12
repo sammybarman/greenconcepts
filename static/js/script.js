d04c27b1f4771237d0fc08413e22f5a9432c7902
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2019-04-12 22:18:52 +0530

    Added cleint carousel initializer

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,7 @@
 var radio_num = 1;
-
+var cardloc;
 setInterval(check, 8000);
 function check() {
-  console.log($("radio:checked").name);
   document.getElementById("slide"+radio_num.toString()).checked = false;
   radio_num = radio_num + 1;
   if(radio_num > 5){
@@ -13,4 +12,19 @@ function check() {
 
 $(document).ready(function(){
   $('.parallax').parallax();
+  $('.carousel').carousel();
+  cardloc = document.getElementById('cards').getBoundingClientRect();
+  if (isScrolledIntoView == true) {
+     console.log("LOLOLOL");
+  }
+  else {
+    console.log("FALSE");
+  }
 });
+
+function isScrolledIntoView() {
+    var elemTop = cardloc.getBoundingClientRect().top;
+    var elemBottom = cardloc.getBoundingClientRect().bottom;
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
+}
