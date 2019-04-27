851b18c58a9c73961328da5403dcd479cecb0eed
Sammy <sammy@Sammys-MacBook-Pro.local>
2019-04-27 10:15:56 +0530

    Created new pages. Edited server.py to display said pages.

--- server.py
+++ server.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def renderPage():
     return render_template("index.html")
 
+@app.route("/rainwater")
+def rainwater():
+    return render_template("rainwater.html")
+
+@app.route("/wastewater")
+def wastwater():
+    return render_template("wastewater.html")
+
+@app.route("/infrastructure")
+def infrastructure():
+    return render_template("infrastructure.html")
+
+@app.route("/test1")
+def test1():
+    return render_template("test1.html")
+
+@app.route("/test2")
+def test2():
+    return render_template("test2.html")
+
+@app.route("/watermanage")
+def watermanage():
+    return render_template("watermanage.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
